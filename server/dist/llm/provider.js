f8eab059fb5345d3c93770b48ad799fe4a99497d
vloshaj_sfemu <vloshaj@salesforce.com>
2026-03-12 17:33:00 -0400

    Removed dist directory - added incorrectly

--- server/dist/llm/provider.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * LLM provider interface.
- * Abstracts the underlying LLM service for easy provider switching.
- */
-Object.defineProperty(exports, "__esModule", { value: true });
--- server/dist/llm/types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * LLM abstraction layer types.
- * Provider-agnostic interfaces for chat completions with tool use.
- */
-Object.defineProperty(exports, "__esModule", { value: true });
